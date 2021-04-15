8395959377fdfdb5a1003b34a14c64afcf0127fc
ryifkiyidan <62694926+ryifkiyidan@users.noreply.github.com>
2021-04-15 21:24:24 +0700

    4. Membuat Halaman Home

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="mt-4 p-3">
+    <hr>
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                Copyright @<?= date('Y'); ?> Ryifki Yidan
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,49 @@
-<div>
-    <h2>HEYYYYYY</h2>
+<div class="container">
+    <!-- Banner -->
+    <div class="banner">
+        <img src="{{ url('assets/slider/slider1.png') }}" alt="slider">
+    </div>
+
+    <!-- Pilih Liga -->
+    <section class="pilih-liga mt-4">
+        <h3><b>Pilih Liga</b></h3>
+        <div class="row mt-4">
+            @foreach ($ligas as $liga)
+            <div class="col">
+                <div class="card shadow">
+                    <div class="card-body text-center">
+                        <img src="{{ url('assets/liga') }}/{{ $liga->gambar }}" alt="liga" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </section>
+
+    <!-- Best Product -->
+    <section class="best-products mt-5">
+        <h3><b>Best Products</b></h3>
+        <div class="row mt-4">
+            @foreach ($products as $product)
+            <div class="col">
+                <div class="card">
+                    <div class="card-body text-center">
+                        <img src="{{ url('assets/jersey') }}/{{ $product->gambar }}" alt="liga" class="img-fluid">
+                        <div class="row mt-2">
+                            <div class="col-md-12">
+                                <h5><b>{{ $product->nama }}</b></h5>
+                                <h5>Rp{{ number_format($product->harga) }}</h5>
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col-md-12">
+                                <a href="#" class="btn btn-dark btn-block">Detail</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </section>
 </div>
